0fd1577cabc274fff76561b07acace2636a2c6ca
Kuboslavko <169147829+Kuboslavko@users.noreply.github.com>
2024-06-04 20:34:02 +0200

    bluescreen
    update 4.6.2024

--- bluescreen.py
+++ bluescreen.py
@@ -9,28 +9,33 @@ root.configure(bg='blue')
 
 
 label1 = tk.Label(root, text=":(", fg='white')
-label1 = tk.Label(root, text=":(", font=("Arial", 95), bg='blue',fg="white")
+label1 = tk.Label(root, text=":(", font=("Segoe UI", 95), bg='blue',fg="white")
 label1.place(x=80, y=50)
 
 label2 = tk.Label(root, text="Your PC run into a problem, and needs to restart. Were just collecting some error info", font=("Arial", 20), bg='blue',fg="white")
 label2.place(x=80, y=400)
-label2 = tk.Label(root, text="0% complete", font=("Arial", 20), bg='blue',fg="white")
+label2 = tk.Label(root, text="0% complete", font=("Segoe UI", 20), bg='blue',fg="white")
 label2.place(x=80, y=500)
 
 root.mainloop()
 
-transformation = tkinter.PhotoImage(file='qrcode.png')
-tk.Canvas.create_image(x=80, y=600, image=transformation)
-import tkinter
 
-canvas = tkinter.Canvas()
-canvas.pack()
+#obr = Image.open('qrcode.png')
+#obr1 = Image.open('qrcode.png')
+#obr2 = Image.open('qrcode.png')
+#obr1.save('qrcode.png')
 
-obr = tkinter.PhotoImage(file='qrcode.png')
-for x in range(80,):
-    canvas.create_image(x, 150, image=obr)
+#upgrade (stare treba vymazat)
 
-tkinter.mainloop()
+#img = Image.open("qrcode.png")
+#img = img.resize((250, 250), Image.Resampling.NEAREST)
+
+#obr = ImageTk.PhotoImage(img)
+
+#tk.Label(root, image=obr).place(x=80,y=700)
+
+#root.mainloop()
+
 
 
 
